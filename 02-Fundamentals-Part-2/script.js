fcb992796cd7fcdfef880fc2fc524281c6525d7f
Cristian Herrera <cristiandherrera95@gmail.com>
2020-10-29 13:52:39 -0700

    reorganized directory

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,20 @@
+// *********************
+// Acivating Strict Mode
+// *********************
+
+// // Creates visble errors in the dev console to help debugging
+
+// // When in use('use strict') it has reserved key words
+
+// 'use strict';
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if(passTest) hasDriverLicense = true;  //<< ERROR: typo in license variable
+// if(hasDriversLicense) console.log("I can Drive!!");
+
+// // const interface = 'Audio;
+// // const private = 534;
+
+// ==================================================================================== //
