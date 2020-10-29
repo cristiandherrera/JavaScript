1e511a5f36685faf25c652eb1aad240dc2b6de9f
Cristian Herrera <cristiandherrera95@gmail.com>
2020-10-28 19:49:15 -0700

    added test branch and updated challenge.js

--- 01-Fundamentals-Part-1/challenge.js
+++ 01-Fundamentals-Part-1/challenge.js
@@ -99,8 +99,8 @@
 // Coding Challenge #4.
 // *******************
 
-const bill = 275;
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// const bill = 275;
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
 
