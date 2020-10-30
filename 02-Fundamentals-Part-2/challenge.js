0ca844928525ef66107e818888d77c10a93bda2b
Cristian Herrera <cristiandherrera95@gmail.com>
2020-10-30 16:08:07 -0700

    completed challenge #1. on challenge.js

---
+++ 02-Fundamentals-Part-2/challenge.js
@@ -0,0 +1,32 @@
+// *******************
+// Coding Challenge #1.
+// *******************
+
+// // Calculating the avg scores
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+// // Test Data 1
+// const dolphinsData1 = calcAverage(44, 23, 71);
+// const koalasData1 = calcAverage(65, 54, 49);
+// console.log(dolphinsData1, koalasData1);
+
+// // Test Data 2 
+// const dolphinsData2 = calcAverage(85, 54, 41);
+// const koalasData2 = calcAverage(23, 34, 27);
+// console.log(dolphinsData2, koalasData2);
+
+// // Checking winner
+// const checkWinner = function (avgDolphins, avgKoalas) 
+// {
+//   if (avgDolphins >= 2 *avgKoalas) {
+//     console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+//   } else if(avgKoalas >= 2 *avgDolphins) {
+//     console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+//   } else {
+//     console.log(`NO teams won!`)
+//   }
+// }
+// // Test 1
+// checkWinner(dolphinsData1, koalasData1);
+// // Test 2
+// checkWinner(dolphinsData2, koalasData2);
