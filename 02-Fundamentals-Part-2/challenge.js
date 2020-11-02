44dcba3ce42c47bce31e24d52975d3ca633aeed1
Cristian Herrera <cristiandherrera95@gmail.com>
2020-11-02 13:54:06 -0800

    completed Coding Challenge #2

--- 02-Fundamentals-Part-2/challenge.js
+++ 02-Fundamentals-Part-2/challenge.js
@@ -30,3 +30,20 @@
 // checkWinner(dolphinsData1, koalasData1);
 // // Test 2
 // checkWinner(dolphinsData2, koalasData2);
+
+// ===================================================================================== //
+
+// *******************
+// Coding Challenge #2. 
+// *******************
+
+// const calcTip = function (bill) {
+//   const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+//   return tip;
+// }
+// console.log(calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]));
+
+// const bills = [125, 555, 44];
+// const tips = [18.75, 111, 8.8];
+
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
