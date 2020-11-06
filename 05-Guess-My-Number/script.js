79681595ef79dabaf3e98dd5c0cee4e45859047b
Cristian Herrera <cristiandherrera95@gmail.com>
2020-11-05 22:26:30 -0800

    completed Selecting and Manipulating Elements

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -9,3 +9,21 @@
 // // It is NOT part of javascript; but can access html elements and styles and manipulate them with javascript.
 
 // // The modern DOM is built using multiple APIs that work together.
+
+// // ***********************************
+// // Selecting and Manipulating Elements
+// // ***********************************
+
+// // One way to select and access an element with the DOM is with document.querySelector("element").
+
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent = "Correct Number!";
+// console.log(document.querySelector(".message").textContent);
+
+// document.querySelector(".number").textContent = "8";
+// document.querySelector(".score").textContent = "24";
+
+// console.log(document.querySelector(".guess").type);
+// document.querySelector(".guess").type = "text";
+// document.querySelector(".guess").value = "RIP";
+// console.log(document.querySelector(".guess").type);
