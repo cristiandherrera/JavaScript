e5a30c67f76c52d9f2784e1acb7ddc98d2394b7f
Cristian Herrera <cristiandherrera95@gmail.com>
2020-11-05 23:59:07 -0800

    Implementing Game Logic

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -42,3 +42,42 @@
 //     document.querySelector(".message").textContent = "No number!";
 //   }
 // });
+
+// ***********************
+// Implementing Game Logic
+// ***********************
+
+// const secretNumber = Math.trunc(Math.random() * 20) + 1;
+// document.querySelector(".number").textContent = secretNumber;
+// let score = 20;
+
+// document.querySelector(".check").addEventListener("click", function () {
+//   const guess = Number(document.querySelector(".guess").value);
+//   console.log(typeof guess);
+
+//   if (!guess) {
+//     document.querySelector(".message").textContent = "No number!";
+//   } else if (guess === secretNumber) {
+//     document.querySelector(".message").textContent = "Correct Number!";
+//   } else if (guess > secretNumber) {
+//     if (score > 1) {
+//       document.querySelector(".message").textContent = "Too High!";
+//       score--;
+//       document.querySelector(".score").textContent = score;
+//     } else {
+//       score = 0;
+//       document.querySelector(".message").textContent = "You lost the game!";
+//       document.querySelector(".score").textContent = 0;
+//     }
+//   } else if (guess < secretNumber) {
+//     if (score > 1) {
+//       document.querySelector(".message").textContent = "Too Low!";
+//       score--;
+//       document.querySelector(".score").textContent = score;
+//     } else {
+//       score = 0;
+//       document.querySelector(".message").textContent = "You lost the game!";
+//       document.querySelector(".score").textContent = 0;
+//     }
+//   }
+// });
