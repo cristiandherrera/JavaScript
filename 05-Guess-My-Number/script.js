1093b7f3771bf2bf7316c07955147dc97d10de63
Cristian Herrera <cristiandherrera95@gmail.com>
2020-11-05 22:23:20 -0800

    completed Whats DOM and DOM Manipulation

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+
+// // **********************************
+// // Whats the DOM and DOM Manipulation
+// // **********************************
+
+// // The DOM(Document Object Model) is an object oriented, structured, represention of html documents.
+
+// // It is NOT part of javascript; but can access html elements and styles and manipulate them with javascript.
+
+// // The modern DOM is built using multiple APIs that work together.
