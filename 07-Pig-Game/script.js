6e91152dc5f4842030c8309ae93f8e0a2eb14f91
Cristian Herrera <cristiandherrera95@gmail.com>
2020-11-07 14:36:49 -0800

    set starting conditions for player scores. created css class hidden

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,1 +1,15 @@
-'use strict';
+"use strict";
+
+// *******************
+// Project 3: Pig Game
+// *******************
+
+// Selecting elements
+const diceElement = document.querySelector(".dice");
+const scoreElement0 = document.querySelector("#score--0");
+const scoreElement1 = document.querySelector("#score--1");
+
+// Setting starting coditions
+diceElement.classList.add("hidden");
+scoreElement0.textContent = 0;
+scoreElement1.textContent = 0;
