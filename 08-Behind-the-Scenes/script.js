93a8a3ea7d6bdf55d7cfa4bfe1dbf394d31ee5b8
Cristian Herrera <cristiandherrera95@gmail.com>
2020-11-09 11:02:15 -0800

    notes on javascipt at a high level

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,1 +1,38 @@
-'use strict';
+"use strict";
+
+// ************************************
+// An High-Level Overview of Javascript
+// ************************************
+
+// Javascript is a high-level, prototype-based object oriented, multi-paradigm, interpreted or just-in-time compiled, dynamic, single threaded, garbage-collected programming language with first-class funtions and a non blocking event loop concurrency model!!!
+
+// High-Level
+//  - DOES NOT have to manually ask the computer for memory resources when creating a variable.
+//  - These are called abstractions, they take all the work away from us. Makeing it nice and easy to learn/use!!
+//  - Downside? Yes, not nearly as fast or as optimized as low level languages like C.
+
+// Garbage-collected
+//  - Is a memory management tool; is an algorithim inside of the javascript engine that removes old unused objects from computer memory.
+
+// Interpreted or Just-in-Time Compiled language
+//  - Converts our javascript into machine code(0s & 1s) inside the javascript engine
+
+// Multi-Paradigm
+//  - A paradigm is an approach and midset of structuring code, which will direct your coding style and technique.
+//    * Procedural programming
+//    * Object-oriented programming(OOP)
+//    * Funcitonal programming(FP)
+
+// Prototype-based object-oriented
+//  - Contains prewrtten object blue prints that have yet to have its classes defined.
+
+// First-class functions
+//  - Functions are simply treated as variables. We can pass them into other functions, and return them from functions.
+
+// Dynamic
+//  - Refers to Dynamically typed laguages: We do not have to define the datatype when declaring variables and can be automatically changed.
+
+// Non-Blocking Event Loop Conurrency Model in a Single Thread
+//  - concurrency model: how the javascript engine handles mulitple tasks happening at the same time.
+//  - Javascript runs on a single thread; can only do one thing at a time.
+//  - Event loop takes long running tasks and executes them in the 'background', and puts them on main thread when finished!
