204dcdc2cbdecd01259f24fbddc2ab1c38bcd3d9
Cristian Herrera <cristiandherrera95@gmail.com>
2020-11-21 21:35:27 -0800

    working with strings - part 2

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -75,6 +75,72 @@ const restaurant = {
 // ================================================================================================== //
 
 // *****************************
+// Working with Strings - Part 2
+// *****************************
+
+// const airline = "TAP Air Portugal";
+
+// console.log(airline.toLowerCase());
+// console.log(airline.toUpperCase());
+// console.log("Cristian".toUpperCase());
+
+// const passenger = "crIsTiaN";
+// const passengerLower = passenger.toLowerCase();
+// const passengerCorrect =
+//   passengerLower[0].toUpperCase() + passengerLower.slice(1);
+// console.log(passengerCorrect);
+
+// const email = "hello@cristian.com";
+// const loginEmail = "Hello@Cristian.cOM \n";
+
+// // const lowerEmail = loginEmail.toLowerCase();
+// // const trimmedEmail = lowerEmail.trim();
+// // console.log(trimmedEmail); // hello@cristian.com
+
+// // returns string so we can chain string methods
+// const normlizedEmail = loginEmail.toLowerCase().trim();
+// console.log(normlizedEmail);
+// console.log(email === normlizedEmail);
+
+// // replacing parts of strings
+// const priceGB = "288,97E";
+// const priceUS = priceGB.replace("E", "$").replace(",", ".");
+// console.log(priceUS);
+
+// // 'replace() creates brand new string does NOT mutate orignal string
+// const announcment =
+//   "All passengers come to boarding door 23. Boarding door 23.";
+
+// console.log(announcment.replace("door", "gate"));
+
+// // using a regular expression to target ALL strings with 'door'
+// console.log(announcment.replace(/door/g, "gate"));
+
+// // Booleans
+// const plane = "Airbus A320neo";
+// console.log(plane.includes("A320neo"));
+// console.log(plane.includes("Boeing"));
+// console.log(plane.startsWith("Airb"));
+
+// if (plane.startsWith("Airbus") && plane.endsWith("neo")) {
+//   console.log("Part of the NEW Airbus family");
+// }
+
+// // Practice
+// const checkBaggage = function (items) {
+//   const baggage = items.toLowerCase();
+//   if (baggage.includes("knife") || baggage.includes("gun")) {
+//     console.log("You have just been ARRESTED");
+//   } else {
+//     console.log("Welcome aboard!");
+//   }
+// };
+
+// checkBaggage("I have a labtop, some Food and a pocket Knife.");
+// checkBaggage("Socks and camera.");
+// checkBaggage("Got some snacks and a gun for protection.");
+
+// *****************************
 // Working with Strings - Part 1
 // *****************************
 
