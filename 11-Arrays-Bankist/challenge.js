fcad6804a1e88da7a0124635f08b52526482edf9
Cristian Herrera <cristiandherrera95@gmail.com>
2020-12-01 11:01:42 -0800

    coding challenge 1

---
+++ 11-Arrays-Bankist/challenge.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// *******************
+// Coding Challenge #1
+// *******************
+
+// // DATA 1:
+// const dogsJulia1 = [3, 5, 2, 12, 7];
+// const dogsKate1 = [4, 1, 15, 8, 3];
+
+// // DATA 2:
+// const dogsJulia2 = [9, 16, 6, 8, 3];
+// const dogsKate2 = [10, 5, 6, 1, 4];
+
+// // 1.
+// const checkdogs = function (arr1, arr2) {
+//   // 2.
+//   const newArr = arr1.slice();
+//   newArr.splice(0, 1);
+//   newArr.splice(-2);
+//   console.log(newArr);
+//   // 3.
+//   const concatArr = newArr.concat(arr2);
+//   console.log(concatArr);
+//   // 4.
+//   concatArr.forEach(function (age, i) {
+//     if (age >= 3) {
+//       console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`);
+//     } else {
+//       console.log(`Dog number ${i + 1} is still a puppy!`);
+//     }
+//   });
+// };
+
+// // 5.
+// checkdogs(dogsJulia1, dogsKate1);
+// checkdogs(dogsJulia2, dogsKate2);
