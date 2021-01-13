a4692858131c1fbb7b458e968dddfb69470e0c8f
Cristian Herrera <cristiandherrera95@gmail.com>
2021-01-12 23:40:04 -0800

    update

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,1 +1,15 @@
-'use strict';
+"use strict";
+
+// free code camp
+
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  }
+
+  return "Not Found";
+  // Only change code above this line
+}
+
+console.log(checkObj({ gift: "pony", pet: "kitten", bed: "sleigh" }, "gift"));
