993a73780fd7bb40366da34a2f22308d5942e462
Cristian Herrera <cristiandherrera95@gmail.com>
2021-01-14 22:38:17 -0800

    What is Object-Oriented Programming?

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,15 +1,33 @@
 "use strict";
 
-// free code camp
+// ***********************************
+// What is Object-Oriented Programming
+// ***********************************
 
-function checkObj(obj, checkProp) {
-  // Only change code below this line
-  if (obj.hasOwnProperty(checkProp)) {
-    return obj[checkProp];
-  }
+/*
+ What is Object-Oriented Programming?
 
-  return "Not Found";
-  // Only change code above this line
-}
+   Object-oriented programming (OOP) is programming paradigm based on the concept of objects.
 
-console.log(checkObj({ gift: "pony", pet: "kitten", bed: "sleigh" }, "gift"));
+   We use objects  to model (describe) real-world or abstract features.
+
+   Objects may contain data (properties) and code (methods). By using objects, we pack data and the corresponding behavior into one block.
+
+   In OOP, objects are self-contained pieces/blocks of code.
+
+   Objects are building blocks of applications, and interact with one another
+
+   Interaction happen through a public interface (API): methods that the code outside of the object can acess and use to communicate with the object.
+
+   OOP was developed with the goal of organizing code, to make it more flexible and easier to maintain (avoid "spaghetti code").
+
+ The 4 Fumdemntal OOP Principle
+
+   1. Abstraction: Ignoring or hiding details that don't matter, allowing us to get an overview perspective of the thing we're implementing, instead of messing with details that don't really matter to our implementation.
+
+   2. Encapsulation: Keeping properties and methods private inside the class, so they are not accessible from outside the class. Some methods can be exposed as a public interface(API).
+
+   3. Inheritance: Making all properties of a certain class available to a child class, forming a hierarchical relationship between classes. This alows us to reuse common logic and to model real-world relationships.
+
+   4. Polymorphism: A child class can overwrite a method it inherited from a parent class [it's more complex than that, but enough for our purposes]
+*/
