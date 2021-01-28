eab1867f7d22cad99c1c95cbcf5d523e6c548c88
Cristian Herrera <cristiandherrera95@gmail.com>
2021-01-27 23:54:32 -0800

    Prototypes (start)

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -9,7 +9,7 @@
 
    - Object-oriented programming (OOP) is a programming paradigm based on the concept of objects.
 
-   - We use objects to model (describe) real-world or abstract features (HTML componenet or data structure).
+   - We use objects to model (describe) real-world or abstract features (HTML component or data structure).
 
    - Objects may contain data (properties) and code (methods). By using objects, we pack data and the corresponding behavior into one block.
 
@@ -17,7 +17,7 @@
 
    - Objects are BUILDING BLOCKS of applications, and INTERACT with one another.
 
-   - Interactions happen through a public interface (API): methods that the code outside of the object can acess and use to communicate with the object.
+   - Interactions happen through a public interface (API): methods that the code outside of the object can access and use to communicate with the object.
 
    - OOP was developed with the goal of ORGANIZING code, to make it more flexible and easier to maintain (avoid "spaghetti code").
 
@@ -29,7 +29,7 @@
 
    - All objects created through a class are called 'instances' of that class. 
 
-     Instantiation: The proccess of creating an instance. (classes creating objects)
+     Instantiation: The process of creating an instance. (classes creating objects)
 
    NOTE: An 'instance' is a real object that we can use in our code, which was created from a 'class', and a class itself is not an object.
    
@@ -40,7 +40,7 @@
 
    2. Encapsulation: Keeping properties and methods private inside the class, so they are not accessible from outside the class. Some methods can be exposed as a public interface(API).
 
-   3. Inheritance: Making all properties of a certain class available to a child class, forming a hierarchical relationship between classes. This alows us to reuse common logic and to model real-world relationships.
+   3. Inheritance: Making all properties of a certain class available to a child class, forming a hierarchical relationship between classes. This allows us to reuse common logic and to model real-world relationships.
 
    4. Polymorphism: A child class can overwrite a method it inherited from a parent class [it's more complex than that, but enough for our purposes]
   
@@ -62,7 +62,7 @@
 
      We can also say that objects delegate behavior (methods) to the linked prototype object. So besides prototypal inheritance, we also call this mechanism, delegation.
 
-     COMPARISON: Prototypal inheritance is DIFFERENT from the 'inheritence' principle on class implementation learned in the previous lecture. In classical OOP with classes, the behaviors (methods) are actually copied from the class to the object and so that is completely different. So that was one class inheriting from another class. But in this case, it's basically an instance inheriting from a class.
+     COMPARISON: Prototypal inheritance is DIFFERENT from the 'inheritance' principle on class implementation learned in the previous lecture. In classical OOP with classes, the behaviors (methods) are actually copied from the class to the object and so that is completely different. So that was one class inheriting from another class. But in this case, it's basically an instance inheriting from a class.
 
  In practice, how do we implement OOP?
   
@@ -73,7 +73,7 @@
 
    2. ES6 Classes
    - Modern alternative to constructor function syntax.
-   - "Syntactic surgar": behind the scenes, ES6 classes work exactly like constuctor functions. (just a nicer syntax)
+   - "Syntactic sugar": behind the scenes, ES6 classes work exactly like constructor functions. (just a nicer syntax)
    - ES6 classes do NOT behave like classes in "classical OOP".
 
    3. Object.create()
@@ -119,12 +119,13 @@ const Person = function (firstName, birthYear) {
   this.birthYear = birthYear;
 
   // Never create a method in a constructor function - POOR PERFORMANCE
+
   // this.calcAge = function () {
   //   console.log(2037 - this.birthYear);
   // };
 };
 
-// 'cristian' is an instance of 'Person'
+// Creating a object INSTANCE of 'Person' named 'cristian'
 const cristian = new Person("Cristian", 1995);
 console.log(cristian);
 
@@ -134,7 +135,7 @@ console.log(cristian);
    1. new empty object is created {}
    2. function is calls, 'this' = {}
    3. {} linked to prototype
-   4. function automatically return {} 
+   4. function automatically return {}
 */
 
 const matilda = new Person("Matilda", 1996);
@@ -142,3 +143,17 @@ const jack = new Person("Jack", 2017);
 console.log(matilda, jack);
 
 console.log(cristian instanceof Person);
+
+// **********
+// Prototypes
+// **********
+
+/*
+ 
+*/
+
+// Prototypes
+
+// const cristian = new Person("Cristian", 1995);
+// const matilda = new Person("Matilda", 1996);
+// const jack = new Person("Jack", 2017);
