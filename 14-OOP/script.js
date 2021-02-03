7ec14d68cad204ff0cacba215bc92f3eff17c2e2
Cristian Herrera <cristiandherrera95@gmail.com>
2021-02-02 23:43:08 -0800

    Built-In Objects (start)

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -300,3 +300,86 @@
 
 // console.log(Person.prototype.constructor === Person); // /TRUE
 // console.log(Person.prototype === Person.prototype.constructor.prototype); // TRUE
+
+// ******************************************
+// Prototypal Inheritance on Built-In Objects
+// ******************************************
+
+/* 
+ 
+*/
+
+// // USING FROM PREVIOUS SECTION
+// const Person = function (firstName, birthYear) {
+//   // Instance properties: will be available on all instances that are created through this function.
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+
+//   // NEVER create a method in a constructor function - POOR PERFORMANCE
+//   this.calcAgeBad = function () {
+//     console.log(2020 - this.birthYear);
+//   };
+// };
+
+// // Creating a object INSTANCE of 'Person' named 'cristian'
+// const cristian = new Person("Cristian", 1995);
+// const matilda = new Person("Matilda", 1996);
+// const jack = new Person("Jack", 2017);
+
+// // Setting a method on the prototype
+// Person.prototype.calcAgeGood = function () {
+//   console.log(2020 - this.birthYear);
+// };
+
+// Setup
+var collection = {
+  2548: {
+    albumTitle: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    albumTitle: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    albumTitle: "ABBA Gold",
+  },
+};
+
+// Only change code below this line
+function updateRecords(object, id, prop, value) {
+  // 1.
+  if (prop != "tracks" && value != "") {
+    object[id][prop] = value;
+  }
+  // 2.
+  if (prop === "tracks" && !object[id].hasOwnProperty("tracks")) {
+    object[id][prop] = [value];
+  }
+  // 3;
+  if (prop === "tracks" && value != "") {
+    object[id][prop].push(value);
+  }
+  // 4;
+  if (value === "") {
+    delete object[id][prop];
+  }
+
+  return object;
+}
+
+var myArray = [];
+
+let i = 5;
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
+
+console.log(myArray);
