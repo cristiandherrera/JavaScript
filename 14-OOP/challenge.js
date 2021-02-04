6194a3b1609dc7ce61f17130a3e8847fd8a53240
Cristian Herrera <cristiandherrera95@gmail.com>
2021-02-03 23:59:40 -0800

    Coding Challenge 1

--- 14-OOP/challenge.js
+++ 14-OOP/challenge.js
@@ -1,1 +1,39 @@
 "use strict";
+
+// *******************
+// Coding Challenge #1
+// *******************
+
+// 1.
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+const car1 = new Car("BMW", 120);
+const car2 = new Car("Mercedes", 95);
+console.log(car1, car2);
+console.log("");
+
+// 2.
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this);
+};
+// 3.
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this);
+};
+
+// 4.
+
+car1.accelerate();
+car1.accelerate();
+car1.brake();
+
+console.log("");
+
+car2.accelerate();
+car2.accelerate();
+car2.brake();
