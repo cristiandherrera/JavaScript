17f3d6eeee7e7777c8500ac01efed6ce0fed0ef4
Cristian Herrera <cristiandherrera95@gmail.com>
2021-02-23 14:45:37 -0800

    Callback Hell

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -5,6 +5,95 @@ const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
 
+// ************************
+// Welcome to Callback Hell
+// ************************
+
+/*
+ What is callback hell?
+
+   Callback hell is when we have a lot of nested callbacks in order to execute asynchronous tasks in sequence.
+
+ Why is it so bad?
+  
+   The problem with callback hell is that it makes our code look very messy. But more importantly, it makes our code harder to maintain, and very difficult to understand. Causing bugs that just create bad code.
+
+ Can we do anything about it?
+
+   Fortunately, since ES6, there is actually a way of escaping callback hell by using something called promises.
+
+ REMEMBER: 
+
+   Callback functions: are just the name of a convention for using JavaScript functions. In JS, the way to create a callback function is to pass it as a parameter to another function, and then to call it back right after something has happened or some task is completed. 
+
+ BELOW: 
+   
+   'request2' is an anonymous callback function NESTED inside the 'request' anonymous callback function
+*/
+
+const renderCountry = function (data, className = "") {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} million people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+`;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbor = function (country) {
+  // AJAX call country (1)
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+
+  // NESTED CALLBACK FUNCTION
+  request.addEventListener("load", function () {
+    // Render country (1)
+    const [data] = JSON.parse(this.responseText);
+    renderCountry(data);
+
+    // Get neighbor country (2)
+    const [neighbor] = data.borders;
+    if (!neighbor) return;
+
+    // AJAX call country (2)
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", `https://restcountries.eu/rest/v2/alpha/${neighbor}`);
+    request2.send();
+
+    // NESTED CALLBACK FUNCTION
+    request2.addEventListener("load", function () {
+      // Render country (2)
+      const data2 = JSON.parse(this.responseText);
+      renderCountry(data2, "neighbour");
+    });
+  });
+};
+getCountryAndNeighbor("usa");
+
+// Callback hell w/ timeouts
+setTimeout(() => {
+  console.log("1 second passed");
+  setTimeout(() => {
+    console.log("2 second passed");
+    setTimeout(() => {
+      console.log("3 second passed");
+      setTimeout(() => {
+        console.log("4 second passed");
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
 // *****************************************
 // How the Web Works: Requests and Responses
 // *****************************************
@@ -65,37 +154,37 @@ const countriesContainer = document.querySelector(".countries");
  NOTE: For APIs, an "endpoint" can include a URL of a server or service. Each endpoint is the location from which APIs can access the resources they need to carry out their function.
 */
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open("GET", `https://restcountries.eu/rest/v2/name/${country}`);
-  request.send();
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open("GET", `https://restcountries.eu/rest/v2/name/${country}`);
+//   request.send();
 
-  request.addEventListener("load", function () {
-    console.log(this.responseText);
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//   request.addEventListener("load", function () {
+//     console.log(this.responseText);
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    const html = `
-    <article class="country">
-      <img class="country__img" src="${data.flag}" />
-      <div class="country__data">
-        <h3 class="country__name">${data.name}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(
-          +data.population / 1000000
-        ).toFixed(1)} million people</p>
-        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-      </div>
-    </article>
-  `;
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
-  });
-};
-getCountryData("usa");
-getCountryData("portugal");
-getCountryData("germany");
+//     const html = `
+//     <article class="country">
+//       <img class="country__img" src="${data.flag}" />
+//       <div class="country__data">
+//         <h3 class="country__name">${data.name}</h3>
+//         <h4 class="country__region">${data.region}</h4>
+//         <p class="country__row"><span>👫</span>${(
+//           +data.population / 1000000
+//         ).toFixed(1)} million people</p>
+//         <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//         <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//       </div>
+//     </article>
+//   `;
+//     countriesContainer.insertAdjacentHTML("beforeend", html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+// getCountryData("usa");
+// getCountryData("portugal");
+// getCountryData("germany");
 
 // ***************************************
 // Asynchronous JavaScript, AJAX, and APIs
