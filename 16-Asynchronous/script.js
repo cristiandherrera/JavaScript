130508c7738806286458602f32e283b93d14377a
Cristian Herrera <cristiandherrera95@gmail.com>
2021-03-04 14:21:46 -0800

    commenting out top code

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -1,29 +1,29 @@
 "use strict";
 
-const btn = document.querySelector(".btn-country");
-const countriesContainer = document.querySelector(".countries");
+// const btn = document.querySelector(".btn-country");
+// const countriesContainer = document.querySelector(".countries");
 
-const renderCountry = function (data, className = "") {
-  const html = `
-  <article class="country ${className}">
-    <img class="country__img" src="${data.flag}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} million people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-    </div>
-  </article>
-`;
-  countriesContainer.insertAdjacentHTML("beforeend", html);
-};
+// const renderCountry = function (data, className = "") {
+//   const html = `
+//   <article class="country ${className}">
+//     <img class="country__img" src="${data.flag}" />
+//     <div class="country__data">
+//       <h3 class="country__name">${data.name}</h3>
+//       <h4 class="country__region">${data.region}</h4>
+//       <p class="country__row"><span>👫</span>${(
+//         +data.population / 1000000
+//       ).toFixed(1)} million people</p>
+//       <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//     </div>
+//   </article>
+// `;
+//   countriesContainer.insertAdjacentHTML("beforeend", html);
+// };
 
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText("beforeend", msg);
-};
+// const renderError = function (msg) {
+//   countriesContainer.insertAdjacentText("beforeend", msg);
+// };
 
 ///////////////////////////////////////
 
@@ -94,10 +94,10 @@ const renderError = function (msg) {
 //     });
 // };
 
-// Creating button
-btn.addEventListener("click", function () {
-  getCountryData("usa");
-});
+// // Creating button
+// btn.addEventListener("click", function () {
+//   getCountryData("usa");
+// });
 // getCountryData("dkjahjk"); //=> ERROR 404
 
 // // NOT CLEAN
